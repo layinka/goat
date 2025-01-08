74bdd27fbca76c112e5585123d1992bfb01b39b3
Filippos Lymperopoulos <filippos@paella.dev>
2025-01-08 02:28:16 +0200

    checks

--- typescript/packages/wallets/crossmint/src/currencies.ts
+++ typescript/packages/wallets/crossmint/src/currencies.ts
@@ -22,14 +22,13 @@ export const EVM_SUPPORTED_CURRENCIES = [
     "sfuel",
 ] as const;
 
-export const OTHER_CHAIN_SUPPORTED_CURRENCIES = [
-    "sui",
-    "sei",
-    "ada",
-    "apt",
-] as const;
+export const OTHER_CHAIN_SUPPORTED_CURRENCIES = ["sui", "sei", "ada", "apt"] as const;
 
-export const SUPPORTED_CURRENCIES = [...EVM_SUPPORTED_CURRENCIES, ...SOLANA_SUPPORTED_CURRENCIES, ...OTHER_CHAIN_SUPPORTED_CURRENCIES] as const;
+export const SUPPORTED_CURRENCIES = [
+    ...EVM_SUPPORTED_CURRENCIES,
+    ...SOLANA_SUPPORTED_CURRENCIES,
+    ...OTHER_CHAIN_SUPPORTED_CURRENCIES,
+] as const;
 
 export type SupportedEVMCurrency = (typeof EVM_SUPPORTED_CURRENCIES)[number];
 export type SupportedSolanaCurrency = (typeof SOLANA_SUPPORTED_CURRENCIES)[number];
